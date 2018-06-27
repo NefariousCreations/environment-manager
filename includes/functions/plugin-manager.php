66b415dfdbe419e7586c34790a30bc5453fb5247
Corey Crowden <corey@nefariouscreations.com.au>
2018-06-27 13:38:18 +1000

    Moved plugin activation remove from blacklist function to hook after a plugin is activated

--- includes/functions/plugin-manager.php
+++ includes/functions/plugin-manager.php
@@ -32,36 +32,12 @@ function runBlacklistPluginActivation() {
         // Check for the current environment
         if (get_site_url() === get_sub_field('environment_site_url', 'option')):
 
-          // Define the environment name
-          $current_environment_name = get_sub_field('environment_name');
-
           /**
            * Check for plugin deactivation POST and add deactivated plugin to blacklist
            */
           if (isset($_GET['action']) && $_GET['action'] === 'deactivate') :
             if (isset($_GET['plugin'])):
               add_sub_row('environment_blacklisted_plugins', ['environment_blacklisted_plugin_name' => $_GET['plugin']]);
-            endif;
-          endif;
-
-          /**
-           * Check for plugin activation POST and remove activated plugin from blacklist
-           */
-          if (isset($_GET['action']) && $_GET['action'] === 'activate') :
-            if (isset($_GET['plugin'])):
-
-              // Get the index (ID) of the rows for the plugin that is to be activated
-              while(have_rows('environment_blacklisted_plugins')): the_row();
-                if ($_GET['plugin'] === get_sub_field('environment_blacklisted_plugin_name')):
-                  $plugin_row_to_remove_from_blacklist = get_row_index();
-                endif;
-              endwhile;
-
-              // If the plugin to be activated is blacklisted, remove it from the blacklist
-              if ($plugin_row_to_remove_from_blacklist):
-                delete_sub_row('environment_blacklisted_plugins', $plugin_row_to_remove_from_blacklist);
-              endif;
-
             endif;
           endif;
 
@@ -111,3 +87,58 @@ add_action('admin_init', 'runBlacklistPluginActivation');
 
 // Trigger plugin update after a WP MigrateDB Push or Pull
 add_action('wpmdb_migration_complete', 'runBlacklistPluginActivation');
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Remove plugin from the current environment blacklist if the plugin is activated
+ */
+add_action( 'activated_plugin', function ($plugin) {
+  /**
+   * Import required functions from WordPress Admin
+   */
+  if (! function_exists('get_plugins')) {
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+  }
+
+  /**
+   * Get the plugins from the options page
+   */
+
+  // Check if any environments have been set
+  if(have_rows('environment', 'option')):
+
+    while (have_rows('environment', 'option')) : the_row();
+
+      // Get the type of environment select
+      if(get_row_layout() == 'environment_by_url'):
+
+        // Check for the current environment
+        if (get_site_url() === get_sub_field('environment_site_url', 'option')):
+
+          /**
+           * Check for plugin activation POST and remove activated plugin from blacklist
+           */
+
+          // Get the index (ID) of the rows for the plugin that is to be activated
+          while(have_rows('environment_blacklisted_plugins')): the_row();
+            if ($plugin === get_sub_field('environment_blacklisted_plugin_name')):
+              $plugin_row_to_remove_from_blacklist = get_row_index();
+            endif;
+          endwhile;
+
+          // If the plugin to be activated is blacklisted, remove it from the blacklist
+          if ($plugin_row_to_remove_from_blacklist):
+            delete_sub_row('environment_blacklisted_plugins', $plugin_row_to_remove_from_blacklist);
+          endif;
+
+        endif;
+
+      endif;
+
+    endwhile;
+
+  endif;
+
+}, 10, 2 );
+
