45db5cde342847e68ea908311fcc50950e81a0c5
Corey Crowden <corey@nefariouscreations.com.au>
2018-06-28 16:42:27 +1000

    Update some function names, comments

--- includes/functions/plugin-manager.php
+++ includes/functions/plugin-manager.php
@@ -8,7 +8,11 @@
  *
  * @since 0.1.0
  */
-function runBlacklistPluginActivation() {
+function run_environment_plugin_update() {
+
+  if (! current_user_can('activate_plugins')):
+    wp_die(__('You do not have sufficient permissions to activate plugins for this site.'));
+  endif;
 
   /**
    * Import required functions from WordPress Admin
@@ -51,7 +55,7 @@ function runBlacklistPluginActivation() {
            * Disable Blacklisted Plugins And force Enable others Only if a current Environment is Active
            */
 
-          // Remove the black listed plugins from global plugin list
+          // Remove the blacklisted plugins from global plugin list
           $environment_enabled_plugins = array_diff(array_keys(get_plugins()), $current_environment_blacklisted_plugins);
 
           // Remove the already active plugins from the global plugin list
@@ -74,27 +78,17 @@ function runBlacklistPluginActivation() {
 }
 
 // Trigger plugin update on admin page loads
-add_action('admin_init', 'runBlacklistPluginActivation');
+add_action('admin_init', 'run_environment_plugin_update');
 
 // Trigger plugin update after a WP MigrateDB Push or Pull
-add_action('wpmdb_migration_complete', 'runBlacklistPluginActivation');
+add_action('wpmdb_migration_complete', 'run_environment_plugin_update');
 
 // -----------------------------------------------------------------------------
 
 /**
  * Remove plugin from the current environment blacklist if the plugin is activated
  */
-add_action( 'activated_plugin', function ($plugin) {
-  /**
-   * Import required functions from WordPress Admin
-   */
-  if (! function_exists('get_plugins')) {
-    require_once ABSPATH . 'wp-admin/includes/plugin.php';
-  }
-
-  /**
-   * Get the plugins from the options page
-   */
+add_action('activate_plugin', function ($plugin) {
 
   // Check if any environments have been set
   if(have_rows('environment', 'option')):
@@ -107,21 +101,24 @@ add_action( 'activated_plugin', function ($plugin) {
         // Check for the current environment
         if (get_site_url() === get_sub_field('environment_site_url', 'option')):
 
-          /**
-           * Check for plugin activation POST and remove activated plugin from blacklist
-           */
+          $plugins_row_to_remove_from_blacklist = array();
 
           // Get the index (ID) of the rows for the plugin that is to be activated
           while(have_rows('environment_blacklisted_plugins')): the_row();
-            if ($plugin === get_sub_field('environment_blacklisted_plugin_name')):
-              $plugin_row_to_remove_from_blacklist = get_row_index();
+            if ($plugin == get_sub_field('environment_blacklisted_plugin_name')):
+              $plugins_row_to_remove_from_blacklist[] = get_row_index();
             endif;
+
           endwhile;
 
-          // If the plugin to be activated is blacklisted, remove it from the blacklist
-          if ($plugin_row_to_remove_from_blacklist):
+          // If the plugin to be activated is blacklisted, remove it from the blacklist,
+          // check for multiple instances and remove all, the array is reversed to delete
+          // the highest row numbers first to prevent the row numbers changing after deleting a row
+          asort($plugins_row_to_remove_from_blacklist);
+          foreach (array_reverse($plugins_row_to_remove_from_blacklist) as $plugin_row_to_remove_from_blacklist):
             delete_sub_row('environment_blacklisted_plugins', $plugin_row_to_remove_from_blacklist);
-          endif;
+          endforeach;
+
 
         endif;
 
@@ -139,17 +136,7 @@ add_action( 'activated_plugin', function ($plugin) {
 /**
  * Add plugin to the current environment blacklist if the plugin is deactivated
  */
-add_action( 'deactivated_plugin', function ($plugin) {
-  /**
-   * Import required functions from WordPress Admin
-   */
-  if (! function_exists('get_plugins')) {
-    require_once ABSPATH . 'wp-admin/includes/plugin.php';
-  }
-
-  /**
-   * Get the plugins from the options page
-   */
+add_action('deactivated_plugin', function ($plugin) {
 
   // Check if any environments have been set
   if(have_rows('environment', 'option')):
@@ -162,10 +149,15 @@ add_action( 'deactivated_plugin', function ($plugin) {
         // Check for the current environment
         if (get_site_url() === get_sub_field('environment_site_url', 'option')):
 
-          /**
-           * Check for plugin deactivation POST and add deactivated plugin to blacklist
-           */
-          if ($plugin):
+          // Check if the plugin is already on the blacklist (This is for the sake of preventing duplication)
+          while(have_rows('environment_blacklisted_plugins')): the_row();
+            if ($plugin === get_sub_field('environment_blacklisted_plugin_name')):
+              $environment_blacklisted_plugin_name_duplicate = true;
+            endif;
+          endwhile;
+
+          // As long as the plugin isn't already listed on the blacklist add it to the blacklist
+          if ($environment_blacklisted_plugin_name_duplicate !== true):
             add_sub_row('environment_blacklisted_plugins', ['environment_blacklisted_plugin_name' => $plugin]);
           endif;
 
